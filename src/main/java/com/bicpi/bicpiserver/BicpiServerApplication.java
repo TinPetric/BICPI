1c1c50b522801665ed3837033be2ffeddd689fd0
Tin Petric <tp51782@fer.hr>
2022-05-16 19:53:17 +0200

    Initial commit

---
+++ src/main/java/com/bicpi/bicpiserver/BicpiServerApplication.java
@@ -0,0 +1,13 @@
+package com.bicpi.bicpiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicpiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicpiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bicpi/bicpiserver/BicpiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bicpi.bicpiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicpiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
